5fcb1d72be67c5883855e0a912e25b664896fe34
ghostrepo <nj_trece@yahoo.com>
2024-10-17 22:21:45 -0400

    initial commit

---
+++ app/src/main/java/com/example/unknown/Main.java
@@ -0,0 +1,124 @@
+package com.example.unknown;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+//import android.util.Log;
+
+public class Main {
+    public static final String TAG = "Testing";
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+        File in = new File("com/example/unknown/input.txt");
+        File out = null;
+        ArrayList<Integer> output = new ArrayList<>();
+
+        try {
+            output = stringToIntArray(calibrate(in));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        int calibration = sum(output);
+        System.out.println("The sum of all calibration values is: " + calibration);
+
+        try {
+            out = writeToFile(output);
+        } catch (IOException io) {
+            System.out.println("Error: IO Exception. " + io);
+        }
+
+        assert out != null;
+        if (out.exists()) {
+            System.out.println("File created successfully");
+        }
+    }
+
+    public static ArrayList<String> calibrate(File input) throws FileNotFoundException {
+        ArrayList<String> stringArray = new ArrayList<>();
+        Scanner s = new Scanner(input);
+
+        //Parse through each line
+        while (s.hasNextLine()) {
+            //One line of input
+            char[] array = s.nextLine().toCharArray();
+            ArrayList<Character> ch = new ArrayList<>();
+
+            //Create integer array from each line of input
+            for (char c : array) {
+                if (Character.isDigit(c)) {
+                    ch.add(c);
+                }
+            }
+            //Drop middle elements from array
+            for (int i = 0; i < ch.size(); i++) {
+                if (ch.size() > 2) {
+                    ch.remove(1);
+                }
+            }
+            //Add new integer array to main ArrayList
+            System.out.print(ch.toString() + "\n");
+            stringArray.add(ch.toString());
+        }
+
+        return stringArray;
+    }
+
+    public static File writeToFile(ArrayList<Integer> list) throws IOException {
+        File outputFile = new File("com/example/unknown/output.txt");
+        if (outputFile.exists()) {
+            if (outputFile.delete()) {
+                System.out.println("Stale file deleted.");
+            } else {
+                System.out.println("Unable to delete output file.");
+            }
+        }
+
+        FileWriter writer = null;
+        if (outputFile.createNewFile()) {
+            writer = new FileWriter(outputFile, true);
+            for (Integer i: list) {
+                System.out.println(i);
+                writer.write(i + "\n");
+            }
+        } else {
+            System.out.println("Failure to create output file");
+        }
+
+        if (writer != null) {
+            writer.close();
+        }
+        return outputFile;
+    }
+
+    public static ArrayList<Integer> stringToIntArray(ArrayList<String> stringArray) {
+        ArrayList<Integer> intArray = new ArrayList<>();
+
+
+        return intArray;
+    }
+
+    public static Integer sum(ArrayList<Integer> values) {
+        int sum = 0;
+        for (Integer num: values) {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
---
+++ app/src/main/java/com/example/unknown/UnknownActivity.java
@@ -0,0 +1,10 @@
+package com.example.unknown;
+
+import android.util.Log;
+
+public class UnknownActivity {
+    public static final String TAG = "Unknown";
+    public UnknownActivity() {
+        Log.d(TAG, "Main Constructor");
+    }
+}
